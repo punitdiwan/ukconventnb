2f406b1e119dd72aaa5b786de62d66352d91c838
Punit Diwan <punitdiwan@gmail.com>
2026-03-12 14:23:28 +0530

    Optimize the code

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,6 @@ export async function getStaticProps(context) {
   
   return {
     props: { data_header,slider_data },
-    revalidate: 2, // will be passed to the page component as props
+    revalidate: 86400,
   }
 }
